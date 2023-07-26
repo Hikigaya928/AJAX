59faef11a0fb990239ea092856c811d8c7c21b40
Hikigaya928 <470564854@qq.com>
2023-07-26 16:39:18 +0800

    超时与网络异常

--- 代码/3-原生AJAX/server.js
+++ 代码/3-原生AJAX/server.js
@@ -7,44 +7,53 @@ const app = express();
 // 3.创建路由规则
 // request是对请求报文的一个封装
 // response是对请求报文的一个封装
-app.get('/server',(request,response)=>{
+app.get('/server', (request, response) => {
     //设置响应头 设置允许跨域
-    response.setHeader('Access-Control-Allow-Origin','*');
-    
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
     // 设置响应体
     response.send('HELLO AJAX');
 });
 
-app.post('/server',(request,response)=>{
+app.post('/server', (request, response) => {
     //设置响应头 设置允许跨域
-    response.setHeader('Access-Control-Allow-Origin','*');
-    
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
     // 设置响应体
     response.send('HELLO AJAX POST');
 });
 
-app.all('/json-server',(request,response)=>{
+app.all('/json-server', (request, response) => {
     //设置响应头 设置允许跨域
-    response.setHeader('Access-Control-Allow-Origin','*');
-    
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
     // 设置响应体
     // response.send('HELLO AJAX JSON');
     const data = {
-        name:'hjy'
+        name: 'hjy'
     };
     let str = JSON.stringify(data);
     response.send(str);
 });
 
 //针对IE缓存问题的规则
-app.get('/ie',(request,response)=>{
+app.get('/ie', (request, response) => {
     //设置响应头 设置允许跨域
-    response.setHeader('Access-Control-Allow-Origin','*');
-    
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
     // 设置响应体
     response.send('HELLO IE-3');
 });
+
+//延时响应
+app.get('/delay', (request, response) => {
+    //设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    setTimeout(() => {
+        response.send('延时响应');
+    }, 3000);
+});
 // 4.监听端口启动服务
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log('服务已经启动,8000端口监听中....');
 })
